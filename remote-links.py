ac3892b7762dfde27ae202d86a6f97ab2ce58243
tqiu8 <tammy.qiu@yahoo.com>
2018-09-25 01:01:40 -0400

    removed links in feelings

--- remote-links.py
+++ remote-links.py
@@ -1,9 +1,11 @@
 import re
+import json
+import pandas as pd
 
-result = ""
-with open('feelings.json') as f:
-    data = f.read()
-    result = re.sub(r"http\S+", "", data, flags=re.MULTILINE)
-
-file = open('feelings-2.json', 'w')
-file.write(result)
+# result = ""
+# with open('feelings.json') as f:
+#     data = json.load(f)
+#     result = re.sub(r"http\S+", "", data, flags=re.MULTILINE)
+df = pd.read_json('feelings.json')
+df['text'] = df['text'].apply(lambda x: re.sub(r"http\S+", "", x, flags=re.MULTILINE))
+df.to_json('feelings-2.json', orient='records')
